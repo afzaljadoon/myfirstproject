8b9e9ed8cf7721f22d68ece2cd617f40a21af1f4
Afzal Khan Jadoon <116053189+afzaljadoon@users.noreply.github.com>
2023-02-08 09:54:41 +0500

    Added some more function

--- javascript projects/JS Newer Features/app.js
+++ javascript projects/JS Newer Features/app.js
@@ -55,16 +55,16 @@
     // console.log(`Hello ${String(sym1)}`)
 
     // Unique object keys
-    const KEY1 = Symbol();
-    const KEY2 = Symbol('sym2');
+    // const KEY1 = Symbol();
+    // const KEY2 = Symbol('sym2');
     
 
-    const myobj = {};
+    // const myobj = {};
 
-    myobj[KEY1] = 'Prop1';
-    myobj[KEY2] = 'Prop2';
-    myobj.key3 = 'Prop3';
-    myobj.key4 = 'Prop4';
+    // myobj[KEY1] = 'Prop1';
+    // myobj[KEY2] = 'Prop2';
+    // myobj.key3 = 'Prop3';
+    // myobj.key4 = 'Prop4';
 
     // console.log(myobj[KEY1]);
     // console.log(myobj[KEY2]);
@@ -75,7 +75,60 @@
     // }
 
     // Symbols Are ignored By JSON.stringify
-    console.log(JSON.stringify({key : 'prop'}));
-    console.log(JSON.stringify({[Symbol('sym1')] : 'Prop'}))
-    console.log(JSON.stringify({[Symbol('sym1')] : 'Prop'}))
+    // console.log(JSON.stringify({key : 'prop'}));
+    // console.log(JSON.stringify({[Symbol('sym1')] : 'Prop'}))
+    // console.log(JSON.stringify({[Symbol('sym1')] : 'Prop'}))
+
+    // Destructuring Assignment
+//     let a, b;
+//    [a, b] = [100, 200];
+
+//     // Rest Pattern
+//     [a, b, c, ...rest] = [100, 200, 300, 400, 500];
+
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+//     console.log(...rest);
     
+    // ({a, b} = {a:100, b:200, c:300, d:400, e:500});
+
+    // ({a, b, ...rest} = {a:100, b:200, c:300, d:400, e:500});
+
+    // console.log(a);
+    // console.log(b);
+    // console.log(rest);
+
+
+    // Array Destructuring
+    // const people = ['john', 'Meth', 'william'];
+
+    // const [person1, person2, person3] = people;
+
+    // console.log(person1, person2, person3)
+
+    // Parse Array Returnd From Function
+    // function getPeople () {
+    //     return ['john', 'Meth', 'william'];
+    // }
+    // let person1, person2, person3;
+    // [person1, person2, person3] = getPeople();
+
+    // console.log(person1, person2, person3)
+
+    // Object Destructuring
+    const person = {
+        name : 'John Doe',
+        age : 32,
+        city : 'Miami',
+        gender : 'Male',
+        sayHello : function () {
+            console.log('Hello');
+        }
+    }
+    // New ES6 Destructuring
+   const {name, age, city, sayHello} = person;
+   
+   console.log(name, city, age);
+
+   sayHello();
